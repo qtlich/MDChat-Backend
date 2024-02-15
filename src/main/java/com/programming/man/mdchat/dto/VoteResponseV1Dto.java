8861295d15cebe8e5a2e8cb91b70c7496c0d8c44
VladimirYushko <me.for.registering@gmail.com>
2024-02-15 23:04:03 +0200

    last

---
+++ src/main/java/com/programming/man/mdchat/dto/VoteResponseV1Dto.java
@@ -0,0 +1,19 @@
+package com.programming.man.mdchat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoteResponseV1Dto {
+    private Long postId;
+    private Long commentId;
+    private Long voteCount;
+    private Integer lastVoteType;
+    private Boolean upVoted;
+    private Boolean downVoted;
+    private Integer id;
+    private String message;
+}
