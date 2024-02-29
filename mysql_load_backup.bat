398e395592fecad04a2707bb471b334c959d0203
CutieLich <kozakgd8@gmail.com>
2024-02-29 14:00:37 +0200

    update misc

--- mysql_load_backup.bat
+++ mysql_load_backup.bat
@@ -1,5 +1,4 @@
-::
 set MYSQL_ROOT_PASSWORD="iA2YKJH;zED8n@e"
 docker cp dump.sql mdchat_mysql:/opt
-docker exec mdchat_mysql /bin/sh -c "/bin/cat /opt/dump.sql | /bin/mysql -uroot"
+docker exec mdchat_mysql /bin/sh -c "/bin/cat /opt/dump.sql | /bin/mysql -uroot --password=""%MYSQL_ROOT_PASSWORD%"" mdchat"
 
